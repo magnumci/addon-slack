b47c5a794fc8ece6ae02bf2f4235f7959a722d73
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-11-01 20:04:20 -0500

    Disable net connections

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ require "webmock/rspec"
 require "json"
 require "magnum/addon/slack"
 
+WebMock.disable_net_connect!
+
 def fixture_path(filename=nil)
   path = File.expand_path("../fixtures", __FILE__)
   filename.nil? ? path : File.join(path, filename)
