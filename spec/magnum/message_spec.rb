73b205ecee5de2cd0ef1e8d7efcba0534f957a8d
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-11-10 20:13:29 -0600

    Add message spec

---
+++ spec/magnum/message_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+
+describe Magnum::Addons::Slack::Message do
+  describe "#initialize" do
+    it "requires hash" do
+      expect { described_class.new(nil) }.
+        to raise_error ArgumentError, "Hash required"
+    end
+  end
+
+  describe "#to_s" do
+    let(:payload) { JSON.load(fixture("build.json")) }
+    let(:message) { described_class.new(payload) }
+    let(:result)  { message.to_s }
+
+    it "returns a string" do
+      expect(result).to be_an String
+    end
+
+    it "includes build title" do
+      expect(result).to include '[PASS] slack-notify #3 (master - 6f102f22) by Dan Sosedoff'
+    end
+
+    it "includes diff message" do
+      expect(result).to include "Diff: <https://github.com/sosedoff/slack-notify/compare/42f7b7cdfc4b...6f102f22caac>"
+    end
+
+    it "include commit message" do
+      expect(result).to include "Commit: <https://github.com/sosedoff/slack-notify/commit/6f102f22caac46945e16ada4f50df29a70ab2799|Version bump: 0.1.1>"
+    end
+
+    it "includes author" do
+      expect(result).to include "Author: Dan Sosedoff"
+    end
+
+    it "includes duration" do
+      expect(result).to include "Duration: 11s"
+    end
+
+    it "includes view build url" do
+      expect(result).to include "<https://magnum-ci.com/projects/201/builds/8683|View Build>"
+    end
+  end
+end
